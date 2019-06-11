0413dac83b31a7f858343bea8d8455b34bea8800
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-06-10 21:15:05 -0700

    updated the souce codes of summary in sample

--- sample/summary_and_plot_light.py
+++ sample/summary_and_plot_light.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import matplotlib.cm as cm
 from tqdm import trange, tqdm
-from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import adjusted_rand_score, f1_score
 from argparse import ArgumentParser
 from util.config_parser import ConfigParser_with_eval
+import warnings
+warnings.filterwarnings('ignore')
 
 #%% parse arguments
 def arg_check(value, default):
@@ -64,6 +66,18 @@ def _joblib_get_results(names, lengths, c):
         return np.loadtxt("results/" + name + "_" + c + ".txt").reshape((-1, length))
     return Parallel(n_jobs=-1)([delayed(_component)(n, l, c) for n, l in zip(names, lengths)])
 
+def _convert_label(truth, predict, N):
+    converted_label = np.full_like(truth, N)
+    for true_lab in range(N):
+        counted = [np.sum(predict[truth == true_lab] == pred) for pred in range(N)]
+        pred_lab = np.argmax(counted)
+        converted_label[predict == pred_lab] = true_lab
+    return converted_label
+
+def calc_f1_score(truth, predict, N, **kwargs):
+    converted_predict = _convert_label(truth, predict, N)
+    return f1_score(truth, converted_predict, labels=np.unique(converted_predict), **kwargs )
+
 #%%
 if not os.path.exists("figures"):
     os.mkdir("figures")
@@ -101,15 +115,22 @@ resample_times = np.loadtxt("summary_files/resample_times.txt")
 print("Done!")
 
 #%%
-
 letter_ARI = np.zeros(train_iter)
+letter_macro_f1_score = np.zeros(train_iter)
+letter_micro_f1_score = np.zeros(train_iter)
 word_ARI = np.zeros(train_iter)
+word_macro_f1_score = np.zeros(train_iter)
+word_micro_f1_score = np.zeros(train_iter)
 
 #%% calculate ARI
 print("Calculating ARI...")
 for t in trange(train_iter):
     letter_ARI[t] = adjusted_rand_score(concat_l_l, concat_l_r[t])
+    letter_macro_f1_score[t] = calc_f1_score(concat_l_l, concat_l_r[t], letter_num, average="macro")
+    letter_micro_f1_score[t] = calc_f1_score(concat_l_l, concat_l_r[t], letter_num, average="micro")
     word_ARI[t] = adjusted_rand_score(concat_w_l, concat_w_r[t])
+    word_macro_f1_score[t] = calc_f1_score(concat_w_l, concat_w_r[t], word_num, average="macro")
+    word_micro_f1_score[t] = calc_f1_score(concat_w_l, concat_w_r[t], word_num, average="micro")
 print("Done!")
 
 #%% plot ARIs.
@@ -138,6 +159,10 @@ plt.savefig("figures/Resample_times.png")
 
 #%%
 np.savetxt("summary_files/Letter_ARI.txt", letter_ARI)
+np.savetxt("summary_files/Letter_macro_F1_score.txt", letter_macro_f1_score)
+np.savetxt("summary_files/Letter_micro_F1_score.txt", letter_micro_f1_score)
 np.savetxt("summary_files/Word_ARI.txt", word_ARI)
+np.savetxt("summary_files/Word_macro_F1_score.txt", word_macro_f1_score)
+np.savetxt("summary_files/Word_micro_F1_score.txt", word_micro_f1_score)
 with open("summary_files/Sum_of_resample_times.txt", "w") as f:
     f.write(str(np.sum(resample_times)))
--- sample/summary_summary.py
+++ sample/summary_summary.py
@@ -19,22 +19,35 @@ if not os.path.exists("summary_files"):
 #%%
 print("Initialize variables....")
 N = len(dirs)
-tmp = np.loadtxt(label + "/" +  dirs[0] + "/summary_files/resample_times.txt")
+tmp = np.loadtxt(f"{label}/{dirs[0]}/summary_files/resample_times.txt")
 T = tmp.shape[0]
 
-resample_times  = np.empty((N, T))
+resample_times = np.empty((N, T))
 log_likelihoods = np.empty((N, T+1))
-word_ARIs       = np.empty((N, T))
-letter_ARIs     = np.empty((N, T))
+
+letter_ARIs = np.empty((N, T))
+letter_macro_f1_scores = np.empty((N, T))
+letter_micro_f1_scores = np.empty((N, T))
+
+word_ARIs = np.empty((N, T))
+word_macro_f1_scores = np.empty((N, T))
+word_micro_f1_scores = np.empty((N, T))
+
+
 print("Done!")
 
 #%%
 print("Loading results....")
 for i, path in enumerate(dirs):
-    resample_times[i] = np.loadtxt(label + "/" + path + "/summary_files/resample_times.txt")
-    log_likelihoods[i] = np.loadtxt(label + "/" + path + "/summary_files/log_likelihood.txt")
-    word_ARIs[i] = np.loadtxt(label + "/" + path + "/summary_files/Word_ARI.txt")
-    letter_ARIs[i] = np.loadtxt(label + "/" + path + "/summary_files/Letter_ARI.txt")
+    resample_times[i] = np.loadtxt(f"{label}/{path}/summary_files/resample_times.txt")
+    log_likelihoods[i] = np.loadtxt(f"{label}/{path}/summary_files/log_likelihood.txt")
+    letter_ARIs[i] = np.loadtxt(f"{label}/{path}/summary_files/Letter_ARI.txt")
+    letter_macro_f1_scores[i] = np.loadtxt(f"{label}/{path}/summary_files/Letter_macro_F1_score.txt")
+    letter_micro_f1_scores[i] = np.loadtxt(f"{label}/{path}/summary_files/Letter_micro_F1_score.txt")
+    word_ARIs[i] = np.loadtxt(f"{label}/{path}/summary_files/Word_ARI.txt")
+    word_macro_f1_scores[i] = np.loadtxt(f"{label}/{path}/summary_files/Word_macro_F1_score.txt")
+    word_micro_f1_scores[i] = np.loadtxt(f"{label}/{path}/summary_files/Word_micro_F1_score.txt")
+
 print("Done!")
 
 #%%
@@ -61,12 +74,35 @@ plt.ylabel("ARI")
 plt.title("Transitions of the ARI")
 plt.legend()
 plt.savefig("figures/summary_of_ARI.png")
+
+plt.clf()
+plt.errorbar(range(T), word_macro_f1_scores.mean(axis=0), yerr=word_macro_f1_scores.std(axis=0), label="Word macro F1")
+plt.errorbar(range(T), letter_macro_f1_scores.mean(axis=0), yerr=letter_macro_f1_scores.std(axis=0), label="Letter macro F1")
+plt.xlabel("Iteration")
+plt.ylabel("Macro F1 score")
+plt.title("Transitions of the macro F1 score")
+plt.legend()
+plt.savefig("figures/summary_of_macro_F1_score.png")
+
+plt.clf()
+plt.errorbar(range(T), word_micro_f1_scores.mean(axis=0), yerr=word_micro_f1_scores.std(axis=0), label="Word micro F1")
+plt.errorbar(range(T), letter_micro_f1_scores.mean(axis=0), yerr=letter_micro_f1_scores.std(axis=0), label="Letter micro F1")
+plt.xlabel("Iteration")
+plt.ylabel("Micro F1 score")
+plt.title("Transitions of the micro F1 score")
+plt.legend()
+plt.savefig("figures/summary_of_micro_F1_score.png")
 print("Done!")
 
 #%%
 print("Save npy files...")
 np.save("summary_files/resample_times.npy", resample_times)
 np.save("summary_files/log_likelihoods.npy", log_likelihoods)
-np.save("summary_files/word_ARI.npy", word_ARIs)
+
 np.save("summary_files/letter_ARI.npy", letter_ARIs)
+np.save("summary_files/letter_macro_F1.npy", letter_macro_f1_scores)
+np.save("summary_files/letter_micro_F1.npy", letter_micro_f1_scores)
+np.save("summary_files/word_ARI.npy", word_ARIs)
+np.save("summary_files/word_macro_F1.npy", word_macro_f1_scores)
+np.save("summary_files/word_micro_F1.npy", word_micro_f1_scores)
 print("Done!")
