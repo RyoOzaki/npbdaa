ea6296acbe2f2431c927934441b888965e60e7b9
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-12-19 18:22:55 +0900

    update kill command

--- sample/watchdog_runner.sh
+++ sample/watchdog_runner.sh
@@ -2,7 +2,7 @@
 
 killtree() {
     local _pid=$1
-    local _sig=${2:--TERM}
+    local _sig=${2:-KILL}
     kill -stop ${_pid} # needed to stop quickly forking parent from producing children between child killing and parent killing
     for _child in $(ps -o pid --no-headers --ppid ${_pid}); do
         killtree ${_child} ${_sig}
